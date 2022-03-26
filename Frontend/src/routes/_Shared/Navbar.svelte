941ff2bf447329722e37230efa7b997e653d997a
Jean-François Pustay <jef97310@gmail.com>
2022-03-26 13:02:33 +0100

    Added frontend project

---
+++ Frontend/src/routes/_Shared/Navbar.svelte
@@ -0,0 +1,3 @@
+<nav class="navbar bg-primary text-primary-content container mx-auto mt-6 rounded-lg">
+	<a class="btn btn-ghost normal-case text-xl" href="/">Enx.Blog</a>
+</nav>
---
+++ Frontend/src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+	import Navbar from './_Shared/Navbar.svelte';
+</script>
+
+<svelte:head>
+	<script>
+		if (!('theme' in localStorage)) {
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				document.documentElement.setAttribute('data-theme', 'dark');
+			} else {
+				document.documentElement.setAttribute('data-theme', 'bumblebee');
+			}
+		} else {
+			let data = localStorage.getItem('theme');
+			if (data) {
+				document.documentElement.setAttribute('data-theme', data);
+			}
+		}
+	</script>
+</svelte:head>
+
+<Navbar />
+
+<slot />
